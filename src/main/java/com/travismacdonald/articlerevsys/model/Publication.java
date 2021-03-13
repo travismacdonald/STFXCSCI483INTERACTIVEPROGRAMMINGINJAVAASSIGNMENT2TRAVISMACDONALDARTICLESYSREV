5b5d4db60060f728532a82476c7e15d2c9101ebb
travismacdonald <macdonaldtravisc@gmail.com>
2021-03-13 16:03:02 -0400

    Changed project from primefaces 8.0 to primefaces 10.0
    I hate JSF.

---
+++ src/main/java/com/travismacdonald/articlerevsys/model/Publication.java
@@ -0,0 +1,33 @@
+package com.travismacdonald.articlerevsys.model;
+
+import java.io.Serializable;
+
+
+public class Publication implements Serializable {
+
+    private String url;
+
+    private String title;
+
+    public Publication(String url, String title) {
+        this.url = url;
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
---
+++ src/main/java/com/travismacdonald/articlerevsys/model/Recommendation.java
@@ -0,0 +1,19 @@
+package com.travismacdonald.articlerevsys.model;
+
+public enum Recommendation {
+    ACCEPT("Accept"),
+    REJECT("Reject"),
+    MINOR_REV("Minor Revisions"),
+    MAJOR_REV("Major Revisions");
+
+    private final String name;
+
+    private Recommendation(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return this.name;
+    }
+
+}
