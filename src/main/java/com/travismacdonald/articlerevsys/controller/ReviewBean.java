93ff94f618fa1fc21fc203929b2ae175dc46cac6
travismacdonald <macdonaldtravisc@gmail.com>
2021-03-14 15:56:06 -0300

    Added form validation method.
    I hate JSF.

--- src/main/java/com/travismacdonald/articlerevsys/controller/ReviewBean.java
+++ src/main/java/com/travismacdonald/articlerevsys/controller/ReviewBean.java
@@ -26,43 +26,53 @@ import javax.persistence.Enumerated;
 @Named("reviewBean")
 public class ReviewBean implements Serializable {
 
+    private static final String SUBMISSION_ERROR = "Submission Error";
+
     private static final String REVIEW_MSG = "rev-msg";
 
     private Review review = new Review();
 
     private String publicationTitle = "";
-
     private String publicationUrl = "";
+    private String publicationSummary = "";
+
+    private List<String> positives = new ArrayList(Arrays.asList(""));
+    private List<String> negatives = new ArrayList(Arrays.asList(""));
+    private List<String> majorPoints = new ArrayList(Arrays.asList(""));
+    private List<String> minorPoints = new ArrayList(Arrays.asList(""));
+
+    @Enumerated(EnumType.STRING)
+    private Recommendation recommendation;
+    private String reviewerName = "";
 
     public void attemptReviewSubmission() {
         if (validateSubmissionForm()) {
             System.out.println("valid!");
         }
-
     }
 
     public String getReviewerName() {
-        return review.getReviewerName();
+        return reviewerName;
     }
 
     public void setReviewerName(String reviewerName) {
-        review.setReviewerName(reviewerName);
+        this.reviewerName = reviewerName;
     }
 
     public List<String> getMajorPoints() {
-        return review.getMajorPoints();
+        return majorPoints;
     }
 
     public List<String> getMinorPoints() {
-        return review.getMinorPoints();
+        return minorPoints;
     }
 
     public List<String> getPositives() {
-        return review.getPositives();
+        return positives;
     }
 
     public List<String> getNegatives() {
-        return review.getNegatives();
+        return negatives;
     }
 
     public String getPublicationTitle() {
@@ -81,56 +91,61 @@ public class ReviewBean implements Serializable {
         this.publicationUrl = publicationUrl;
     }
 
+    public String getPublicationSummary() {
+        return publicationSummary;
+    }
+
+    public void setPublicationSummary(String publicationSummary) {
+        this.publicationSummary = publicationSummary;
+    }
+
     public void addMajorPoint() {
-        review.getMajorPoints().add("");
+        majorPoints.add("");
         System.out.println("addMajorPointCalled");
     }
 
     public void addMinorPoint() {
-        review.getMinorPoints().add("");
+        minorPoints.add("");
         System.out.println("addMinorPointCalled");
     }
 
     public void addPositive() {
-        review.getPositives().add("");
+        positives.add("");
         System.out.println("addPositiveCalled");
     }
 
     public void addNegative() {
-        review.getNegatives().add("");
+        negatives.add("");
         System.out.println("addNegative called");
     }
 
     public void removeMajorPoint() {
-        int total = review.getMajorPoints().size();
-        review.getMajorPoints().remove(total - 1);
+        int total = majorPoints.size();
+        majorPoints.remove(total - 1);
         System.out.println("REMOVED major");
     }
 
     public void removeMinorPoint() {
-        int total = review.getMinorPoints().size();
-        review.getMinorPoints().remove(total - 1);
+        int total = minorPoints.size();
+        minorPoints.remove(total - 1);
         System.out.println("REMOVED minor");
     }
 
     public void removePositive() {
-        int total = review.getPositives().size();
-        review.getPositives().remove(total - 1);
+        int total = positives.size();
+        positives.remove(total - 1);
         System.out.println("REMOVED Positive");
     }
 
     public void removeNegative() {
-        int total = review.getNegatives().size();
-        review.getNegatives().remove(total - 1);
+        int total = negatives.size();
+        negatives.remove(total - 1);
         System.out.println("REMOVED negatives");
     }
 
     public List<Recommendation> getRecommendations() {
         return Arrays.asList(Recommendation.values());
     }
-
-    @Enumerated(EnumType.STRING)
-    private Recommendation recommendation;
 
     public Recommendation getRecommendation() {
         return recommendation;
@@ -141,57 +156,160 @@ public class ReviewBean implements Serializable {
     }
 
     public boolean maxMajorPointsReached() {
-        return review.getMajorPoints().size() >= AsrConstants.MAX_MAJOR_POINTS;
+        return majorPoints.size() >= AsrConstants.MAX_MAJOR_POINTS;
     }
 
     public boolean maxMinorPointsReached() {
-        return review.getMinorPoints().size() >= AsrConstants.MAX_MINOR_POINTS;
+        return minorPoints.size() >= AsrConstants.MAX_MINOR_POINTS;
     }
 
     public boolean maxPositivesReached() {
-        return review.getPositives().size() >= AsrConstants.MAX_POSITIVES;
+        return positives.size() >= AsrConstants.MAX_POSITIVES;
     }
 
     public boolean maxNegativesReached() {
-        return review.getNegatives().size() >= AsrConstants.MAX_NEGATIVES;
+        return negatives.size() >= AsrConstants.MAX_NEGATIVES;
     }
 
     public boolean minMajorPointsReached() {
-        return review.getMajorPoints().size() <= AsrConstants.MIN_MAJOR_POINTS;
+        return majorPoints.size() <= AsrConstants.MIN_MAJOR_POINTS;
     }
 
     public boolean minMinorPointsReached() {
-        return review.getMinorPoints().size() <= AsrConstants.MIN_MINOR_POINTS;
+        return minorPoints.size() <= AsrConstants.MIN_MINOR_POINTS;
     }
 
     public boolean minPositivesReached() {
-        return review.getPositives().size() <= AsrConstants.MIN_POSITIVES;
+        return positives.size() <= AsrConstants.MIN_POSITIVES;
     }
 
     public boolean minNegativesReached() {
-        return review.getNegatives().size() <= AsrConstants.MIN_NEGATIVES;
+        return negatives.size() <= AsrConstants.MIN_NEGATIVES;
     }
-    
+
+    /**
+     * Validates each field in the review submission. 'res' becomes false when
+     * at least one validate method fails (i.e. returns false). However, it is
+     * not returned right away so that multiple errors can be logged at once,
+     * rather than returning after the first validation error, thereby only
+     * logging the first error.
+     */
     private boolean validateSubmissionForm() {
-        // Return res, instead of returning false in each statment,
-        // so multiple errors can be logged at once (e.g. no title and no url).
         boolean res = true;
-        if (publicationTitle.isBlank()) {
-            showSubmissionError(
-                    "Submission Error", 
-                    "No publication title provided."
-            );
-            res = false;
-        } 
-        if (publicationUrl.isBlank()) {
-            showSubmissionError(
-                    "Submission Error",
-                    "No publication URL provided."
-            );
+
+        if (!validatePublicationTitle()) {
             res = false;
         }
-        
+        if (!validatePublicationUrl()) {
+            res = false;
+        }
+        if (!validatePublicationSummary()) {
+            res = false;
+        }
+        if (!validatePositives()) {
+            res = false;
+        }
+        if (!validateNegatives()) {
+            res = false;
+        }
+        if (!validateMajorPoints()) {
+            res = false;
+        }
+        if (!validateMinorPoints()) {
+            res = false;
+        }
+        if (!validateReviewerName()) {
+            res = false;
+        }
         return res;
+    }
+
+    private boolean validatePublicationTitle() {
+        if (publicationTitle.isBlank()) {
+            showSubmissionError(
+                    SUBMISSION_ERROR,
+                    "Please provide a publication title."
+            );
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validatePublicationUrl() {
+        if (publicationUrl.isBlank()) {
+            showSubmissionError(
+                    SUBMISSION_ERROR,
+                    "Please provide a publication URL."
+            );
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validatePublicationSummary() {
+        if (publicationSummary.isBlank()) {
+            showSubmissionError(
+                    SUBMISSION_ERROR,
+                    "Please provide a publication summary."
+            );
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validatePositives() {
+        if (countNumberOfNonBlankPoints(positives) == 0) {
+            showSubmissionError(
+                    SUBMISSION_ERROR,
+                    "Please provide at least one positive."
+            );
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateNegatives() {
+        if (countNumberOfNonBlankPoints(negatives) == 0) {
+            showSubmissionError(
+                    SUBMISSION_ERROR,
+                    "Please provide at least one negative."
+            );
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateMajorPoints() {
+        if (countNumberOfNonBlankPoints(majorPoints) == 0) {
+            showSubmissionError(
+                    SUBMISSION_ERROR,
+                    "Please provide at least one major point."
+            );
+            return false;
+        }
+        return true;
+    }
+
+    private boolean validateMinorPoints() {
+        if (countNumberOfNonBlankPoints(minorPoints) == 0) {
+            showSubmissionError(
+                    SUBMISSION_ERROR,
+                    "Please provide at least one minor point."
+            );
+            return false;
+        }
+        return true;
+    }
+    
+    private boolean validateReviewerName() {
+        if (reviewerName.isBlank()) {
+            showSubmissionError(
+                    SUBMISSION_ERROR,
+                    "Please provide a reviewer name."
+            );
+            return false;
+        }
+        return true;
     }
 
     private void showSubmissionError(String summary, String details) {
@@ -204,6 +322,16 @@ public class ReviewBean implements Serializable {
                 REVIEW_MSG,
                 msg
         );
+    }
+
+    private int countNumberOfNonBlankPoints(List<String> points) {
+        int nonBlank = 0;
+        for (String point : points) {
+            if (!point.isBlank()) {
+                nonBlank++;
+            }
+        }
+        return nonBlank;
     }
 
 }
