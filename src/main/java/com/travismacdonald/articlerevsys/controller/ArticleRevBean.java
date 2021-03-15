ab4deb47d19ed05ad091f1d99d17823e945ae61c
travismacdonald <macdonaldtravisc@gmail.com>
2021-03-15 10:40:55 -0300

    Added score saving in project
    I hate JSF.

--- src/main/java/com/travismacdonald/articlerevsys/controller/ArticleRevBean.java
+++ src/main/java/com/travismacdonald/articlerevsys/controller/ArticleRevBean.java
@@ -18,13 +18,27 @@ public class ArticleRevBean implements Serializable {
     
     final Map<Integer, Integer> reviewScores = new HashMap();
 
+    private Review curReview = null;
+    private int curScore;
 
-    public Integer getScore(int reviewId) {
-        return reviewScores.get(reviewId);
+    public Review getCurReview() {
+        return curReview;
+    }
+
+    public void setCurReview(Review curReview) {
+        this.curReview = curReview;
+        this.curScore = reviewScores.getOrDefault(curReview.getId(), 0);
+    }
+
+    public int getCurScore() {
+        return curScore;
+    }
+
+    public void setCurScore(int curScore) {
+        this.curScore = curScore;
+        reviewScores.put(curReview.getId(), curScore);
     }
     
-    public void setScore(int reviewId, int score) {
-        reviewScores.put(reviewId, score);
-    }
+    
     
 }
