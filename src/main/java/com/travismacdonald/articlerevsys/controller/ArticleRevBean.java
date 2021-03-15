50ea0949fdea84e569177f087dc95f45f6e79ee7
travismacdonald <macdonaldtravisc@gmail.com>
2021-03-15 08:07:25 -0300

    Added a bit of styling to the home page.
    I hate JSF.

---
+++ src/main/java/com/travismacdonald/articlerevsys/controller/ArticleRevBean.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.travismacdonald.articlerevsys.controller;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named("articleRevBean")
+@ApplicationScoped
+public class ArticleRevBean implements Serializable {
+    
+    final Map<Integer, Integer> reviewScores = new HashMap();
+    
+    public Integer getScore(int reviewId) {
+        return reviewScores.get(reviewId);
+    }
+    
+    public void setScore(int reviewId, int score) {
+        reviewScores.put(reviewId, score);
+    }
+    
+}
